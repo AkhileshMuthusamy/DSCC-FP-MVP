0249eed63c188f11eb0d5d72d942b8fc38c9b200
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-03-23 22:58:20 -0400

    temp

---
+++ ml_model/DSSC-FC-MVP-Timeseries-Forcasting.py
@@ -0,0 +1,13 @@
+
+from typing import List
+from prophet import Prophet
+import pandas as pd
+
+
+storage = __import__("DSCC-FP-MVP-Storage")
+
+# class Forecast:
+
+    # def __init__(self, stock_list: List[str]) -> None:
+
+
